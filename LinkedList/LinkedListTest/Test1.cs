91bbe99639c6f1eee21395e5ef5642da0ba7219e
warrenh123 <huw@stpaulsschool.org.uk>
2026-03-10 09:23:50 +0000

    Making tests

---
+++ LinkedList/LinkedListTest/Test1.cs
@@ -0,0 +1,15 @@
+﻿using LinkedListClasses;
+
+namespace LinkedListTest;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        MyLinkedList list1 = new MyLinkedList();
+        list1.Append(5);
+        Assert.AreEqual(1,list1.GetCount());
+    }
+}
